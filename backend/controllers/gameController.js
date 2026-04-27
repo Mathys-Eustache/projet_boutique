1a1717f5264b8f7d6b8feca9fa3ce1fdc1b05ebf
Mathys.E <mathys.eustache@ynov.com>
2026-04-27 11:57:00 +0200

    server

---
+++ backend/controllers/gameController.js
@@ -0,0 +1,29 @@
+
+const games = require("../data/games.json");
+
+
+const getAllGames = (req, res) => res.send(games)
+
+
+const getGameById = (req, res) => {
+
+    const idDemande = req.params.id;
+    
+
+    const game = games.find(jeu => jeu.id === idDemande);
+    
+    if (!game) {
+
+        res.status(404).json({
+            message: 'Jeu introuvable'
+        });
+    } else {
+        res.status(200).json({
+            message: 'Jeu trouvé',
+            gameData: game
+        });
+    }
+}
+
+
+module.exports = { getAllGames, getGameById }
